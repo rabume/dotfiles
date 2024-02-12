38e1d9e3d9f88835e7b67cd144ae83a6132fa553
rabume <info@rabu.me>
2024-02-12 22:53:38 +0100

    include markdown filetype & exclude .env files from copilot, update plugins

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -18,6 +18,16 @@ return {
 						dismiss = "<C-]>",
 					},
 				},
+				filetypes = {
+					markdown = true, -- overrides default
+					sh = function()
+						if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+							-- disable for .env files
+							return false
+						end
+						return true
+					end,
+				},
 			})
 		end,
 	},
