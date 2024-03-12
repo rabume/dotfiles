5c548f344eb5ecb1ad881124d48bceda75e1fe3b
rabume <info@rabu.me>
2024-03-12 20:02:17 +0100

    add copilot chat plugin

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,34 +1,34 @@
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		lazy = false,
-		config = function()
-			require("copilot").setup({
-				suggestion = {
-					enabled = true,
-					auto_trigger = true,
-					debounce = 75,
-					keymap = {
-						accept = "<M-1>",
-						accept_word = false,
-						accept_line = false,
-						next = "<M-]>",
-						prev = "<M-[>",
-						dismiss = "<C-]>",
-					},
-				},
-				filetypes = {
-					markdown = true, -- overrides default
-					sh = function()
-						if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
-							-- disable for .env files
-							return false
-						end
-						return true
-					end,
-				},
-			})
-		end,
-	}
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    lazy = false,
+    config = function()
+      require('copilot').setup {
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = '<M-1>',
+            accept_word = false,
+            accept_line = false,
+            next = '<M-]>',
+            prev = '<M-[>',
+            dismiss = '<C-]>',
+          },
+        },
+        filetypes = {
+          markdown = true, -- overrides default
+          sh = function()
+            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+              -- disable for .env files
+              return false
+            end
+            return true
+          end,
+        },
+      }
+    end,
+  },
 }
